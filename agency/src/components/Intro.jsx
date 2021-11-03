cf0ef51f3e0ebcf97d0db3457b93717ae19d8957
SJuras <jurasstpko@gmailcom>
2021-11-03 04:07:01 +0100

    v1.1.5 Hero Section completed.

---
+++ agency/src/components/Intro.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import styled from 'styled-components'
+import Woman from '../img/woman.png'
+
+const Container = styled.div`
+  height: calc(100vh - 50px);
+  display: flex;
+  padding: 20px;
+`
+
+const Left = styled.div`
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const Title = styled.h1`
+  font-size: 60px;
+  width: 60%;
+`
+
+const Desc = styled.p`
+  width: 60%;
+  font-size: 20px;
+  margin-top: 20px;
+`
+
+const Info = styled.div`
+  width: 60%;
+  margin-top: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Button = styled.button`
+  cursor: pointer;
+  padding: 15px;
+  background-color: darkblue;
+  color: #fff;
+  border-radius: 10px;
+  font-weight: bold;
+  border: none;
+  letter-spacing: 2px;
+  transition: 0.5s;
+  &:hover {
+    background-color: lightblue;
+  }
+`
+
+const Contact = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const Phone = styled.span`
+  color: #f0667d;
+  font-weight: bold;
+`
+
+const ContactText = styled.span`
+  color: #333;
+  margin-top: 5px;
+`
+
+const Right = styled.div`
+  width: 40%;
+`
+
+const Image = styled.img`
+width: 100%;`
+
+const Intro = () => {
+  return(
+    <Container>
+      <Left>
+        <Title>Adventures in creative age</Title>
+        <Desc>
+          We believe that designing products and services in close
+          partnership with our clients is the only way to have a real impact on
+          their business.
+        </Desc>
+        <Info>
+          <Button>START A PROJECT</Button>
+          <Contact>
+            <Phone>Call Us (012) 366 - 2199</Phone>
+            <ContactText>For any question or concerns</ContactText>
+          </Contact>
+        </Info>
+      </Left>
+      <Right>
+        <Image src={Woman} />
+      </Right>
+    </Container>
+  )
+}
+
+export default Intro
